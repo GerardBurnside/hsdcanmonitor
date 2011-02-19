60ab45a35f04054865e4354a0c33d4491a061689
gerard.burnside@gmail.com <gerard.burnside@gmail.com@ab4f6607-db23-5636-2e4c-4c7b6cc640b4>
2011-02-19 14:25:56 +0000

    Initial commit: See http://prius-touring-club.com/vbf/showpost.php?p=115419&postcount=166 for rough architecture description (in French).

---
+++ src/com/ptc/hsdcanmonitor/CommandScheduler.java
@@ -0,0 +1,179 @@
+package com.ptc.hsdcanmonitor;
+
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.ptc.android.hsdcanmonitor.R;
+import com.ptc.hsdcanmonitor.commands.BackgroundCommand;
+import com.ptc.hsdcanmonitor.commands.CommandResponseObject;
+import com.ptc.hsdcanmonitor.commands.InitCommand;
+
+public class CommandScheduler implements Runnable {
+	protected volatile boolean _keepRunning = true;
+	// Right now let's not monitor at first, later change it to true:
+	protected volatile boolean _runBackgroundCommands = false;
+	// Counter used to cycle through background commands:
+	private volatile int _counter = 0;
+	// Counter of the number of cycles performed:
+	private int _cyclesNumber = 1;
+	private static final int MAX_CYCLES_NUMBER = 10; // == higher value of periodicity configured!
+	// Object used to wake up when sleeping because nothing to do.
+	protected Object _syncObj = new Object();
+	// The concurrentLinkedQueue is overkill here, but it shouldn't hurt:
+	protected ConcurrentLinkedQueue<CommandResponseObject> manualCommands = new ConcurrentLinkedQueue<CommandResponseObject>();
+	protected BlockingQueue<CommandResponseObject> _input;
+	protected ArrayList<BackgroundCommand> _backgroundCommands = new ArrayList<BackgroundCommand>();
+    // List of requests: // TODO read them from a file instead !!!
+    private final String[] mInitCommandList = {
+    	"ATI", "ATSP6", "ATE0", "ATH1", "ATL1"	
+    };
+    private final String[] mBackgroundCommandsList = {
+    	"AT SH 7E0",
+    		"013C3E", // periodicity 3
+    		"21013C49",
+    	"AT SH 7E2", // periodicity 2
+    		"210161626768", // periodicity 2
+    		"21707174878A98", // periodicity 2
+    	"AT SH 7C0",
+    		"2129"
+    };
+    
+    private CommandScheduler() {
+    	// Private for singleton implem.
+		_input = CanInterface.getInstance().getInputQueue();
+		// Build an Array of BackgroundCommands to cycle through
+		// TODO: read them from an xml file instead:
+		for (int k=0; k<mBackgroundCommandsList.length; k++) {
+			// Force to start over if an "AT" command fails:
+			boolean resetIfFails = mBackgroundCommandsList[k].startsWith("AT");
+			BackgroundCommand newCmd = new BackgroundCommand(mBackgroundCommandsList[k], resetIfFails);
+			// ******************* Hardcoded tests for peridocity:
+			if ("013C3E".equals(mBackgroundCommandsList[k]))
+				newCmd.periodicity = 3;
+			else if ("AT SH 7E2".equals(mBackgroundCommandsList[k]))
+				newCmd.periodicity = 2;
+			else if ("210161626768".equals(mBackgroundCommandsList[k]))
+				newCmd.periodicity = 2;
+			else if ("21707174878A98".equals(mBackgroundCommandsList[k]))
+				newCmd.periodicity = 2;
+			// ******************** End hardcoded tests.
+			_backgroundCommands.add(newCmd);
+		}
+    }
+	/**
+	* Singleton implementation:
+	* CommandSchedulerHolder is loaded on the first execution of CommandScheduler.getInstance() 
+	* or the first access to CommandSchedulerHolder.INSTANCE, not before.
+	*/
+	private static class CommandSchedulerHolder { 
+		public static final CommandScheduler INSTANCE = new CommandScheduler();
+	}
+	 
+	public static CommandScheduler getInstance() {
+		return CommandSchedulerHolder.INSTANCE;
+	} 
+
+	public void addManualCommand(CommandResponseObject cmd) {
+		manualCommands.add(cmd);
+		resetAndWakeUp();
+	}
+
+	public void startBackgroundCommands() {
+		_runBackgroundCommands = true;
+		resetAndWakeUp();
+	}
+
+	public void stopBackgroundCommands() {
+		// We will stop after the current command:
+		_runBackgroundCommands = false;
+		// By stopping now, we're losing the last command, is this a big deal?
+		ResponseHandler.getInstance().endLoggingCommands();
+	}
+
+	public boolean isBackgroundMonitoring() {
+		return _runBackgroundCommands;
+	}
+	
+	public void resetAndWakeUp() {
+		// wake up the thread in case it was sleeping:
+		synchronized (_syncObj) {
+			_counter = 0; // Reset background commands cycle.
+			_syncObj.notifyAll();
+		}
+	}
+	
+	private int incrementCommandCounter() {
+		if (_counter >= _backgroundCommands.size()) {
+			_counter = 0; // cycle back at the beginning!
+			if (++_cyclesNumber > MAX_CYCLES_NUMBER) {
+				_cyclesNumber = 1;
+			}
+		}
+		return _counter++; // returned value is not incremented on purpose!
+	}
+
+	@Override
+	public void run() {
+		while (_keepRunning) {
+			// Treat manual commands in priority (those should be few and time-spaced anyway):
+			CommandResponseObject cmd = manualCommands.poll();
+			if (cmd == null) {
+				// No manual cmd to send? let's check for background ones:
+				if (_runBackgroundCommands) {
+					BackgroundCommand bkgCmd;
+					synchronized (_syncObj) {
+						// Get the first command that is supposed to run at that time:
+						do {
+							bkgCmd = _backgroundCommands.get(incrementCommandCounter());
+						}
+						while (_cyclesNumber % bkgCmd.periodicity != 0);
+						// TODO: we should check that the previous command was "AT SH xxx" if needed!
+						// i.e. if we inserted a manual command in between!
+						
+					}
+					// Let's feed this command to the CanInterface:
+					feedInterface(bkgCmd);
+				}
+				else { // No manual or background Commands, let's go to sleep:
+					synchronized (_syncObj) {
+						try {
+							_syncObj.wait();
+						} catch (InterruptedException e) {
+							// TODO ?
+						}
+						// we've been woken up, let's start over:
+						continue;
+					}
+				}
+			}
+			else { // Let's feed this command to the CanInterface:
+				feedInterface(cmd);
+			}
+		}
+	}
+
+	private void feedInterface(CommandResponseObject cmd) {
+		try {
+			_input.put(cmd);
+		} catch (InterruptedException e) {
+			// TODO Try again or abort?
+			CoreEngine.getInstance().setState(CoreEngine.STATE_CONNECTION_LOST); // overkill?
+		}
+	}
+
+	public void stop() {
+		_keepRunning = false;
+	}
+
+	public void startInitCommands() {
+		// TODO Auto-generated method stub
+		for (int k=0; k<mInitCommandList.length; k++) {
+			CommandResponseObject cmd = new InitCommand(mInitCommandList[k]);
+			feedInterface(cmd);
+		}
+		// TODO: We should actually enforce successful responses to init commands:
+		CoreEngine.getInstance().askForToastMessage(R.string.msg_init_done);
+	}
+
+}
---
+++ src/com/ptc/hsdcanmonitor/commands/CommandResponseObject.java
@@ -0,0 +1,101 @@
+package com.ptc.hsdcanmonitor.commands;
+
+import java.util.ArrayList;
+
+import com.ptc.hsdcanmonitor.CanInterface;
+
+/**
+ * 
+ * This class is used to pass a command to the CanInterface
+ * and store the result of this command.
+ * 
+ * @author guinness
+ *
+ */
+public class CommandResponseObject {
+	protected String _command;
+	protected byte[] _response;
+	protected long _duration;
+	protected boolean _timedOut = false; // let's not be pessimistic ;-)
+	// Optional, to notify the sender that the response is available.
+	protected Object _notifyMe;
+
+	public CommandResponseObject(String command) {
+		_command = command;
+	}
+
+	public String getCommand() {
+        return _command;
+	}
+	public byte[] getCommandToSend() {
+		// Add \r\n to all commands before sending:
+        return (_command + "\r\n").getBytes();
+	}
+
+	public void setRawResponse(ArrayList<Byte> resp) {
+		_response = new byte[resp.size()];
+		for (int i = 0; i < resp.size(); i++) {
+			_response[i] = resp.get(i);
+		}
+	}
+
+	public void setSender(Object obj) {
+		_notifyMe = obj;
+	}
+	/**
+	 * Returns true if a sender was notified, false if nobody registered.
+	 */
+	public boolean notifySender() {
+		if (_notifyMe != null) {
+			synchronized (_notifyMe) {
+				_notifyMe.notifyAll();
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public String getResponseString() {
+		if (_response != null)
+			return new String(_response);
+		else return null;
+	}
+
+	public byte[] getResponsePayload() {
+		if (_response != null) {
+			// TODO: only keep the useful bytes !!!
+			return _response;
+		}
+		else return null;
+	}
+
+	public void setDuration(long timeSpent) {
+		_duration = timeSpent;
+	}
+	public long getDuration() {
+		return _duration;
+	}
+	
+	public void timedOut(boolean b) {
+		_duration = CanInterface.MAX_COMMAND_RESPONSE_TIME;
+		_timedOut = b;
+		byte[] unlucky = "Timed Out!".getBytes();
+		if (_response == null) {
+			_response = unlucky;
+		}
+		else {
+			byte[] partial = _response;
+			_response = new byte[partial.length+unlucky.length];
+			for (int k=0; k<partial.length+unlucky.length; k++)
+			{ // _response = partial + unlucky:
+				_response[k] = k<partial.length?
+									partial[k]
+									: unlucky[k-partial.length];
+			}
+		}
+	}
+	public boolean hasTimedOut() {
+		return _timedOut;
+	}
+
+}
